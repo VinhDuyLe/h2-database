2e41bd10da4772cc2edae17daf6749108fb8c906
Vinh Le Duy <vinhduyle@Vinhs-MacBook-Pro.local>
2022-02-18 13:42:27 -0800

    validation and logger

--- src/main/java/com/vinh/h2database/controller/DepartmentController.java
+++ src/main/java/com/vinh/h2database/controller/DepartmentController.java
@@ -2,10 +2,12 @@ package com.vinh.h2database.controller;
 
 import com.vinh.h2database.entity.Department;
 import com.vinh.h2database.service.DepartmentService;
-import com.vinh.h2database.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,13 +16,17 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDeparment(@RequestBody Department department) {
+    public Department saveDeparment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController"); //showing message when run application and post info from user
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
--- src/main/java/com/vinh/h2database/entity/Department.java
+++ src/main/java/com/vinh/h2database/entity/Department.java
@@ -1,9 +1,13 @@
 package com.vinh.h2database.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Department {
@@ -11,6 +15,12 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    //using validation dependency
+    @NotBlank(message = "Please add department Name")
+    @Length(max =5, min =1)
+    // @Size(max =10, min =0) @Email @Positive @Negative @PositiveOrZero @NegativeOrZero
+    // @Future @FutureOrPresent @Past @PastOrPresent
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
