b62641094c16131aa30a1eb2b9c1fe011b084bd7
vinhduyle <leduyvinh91@gmail.com>
2022-02-20 20:14:32 -0800

    configAndProfiles

--- src/main/java/com/vinh/h2database/H2DatabaseApplication.java
+++ src/main/java/com/vinh/h2database/H2DatabaseApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class H2DatabaseApplication {
 
 	public static void main(String[] args) {
+		if (args.length != 0) {
+			System.out.println(args[0]);
+		}
 		SpringApplication.run(H2DatabaseApplication.class, args);
 	}
 
--- src/main/java/com/vinh/h2database/controller/HelloController.java
+++ src/main/java/com/vinh/h2database/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.vinh.h2database.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String hello() {
-        return "Welcome to new Application";
+        return welcomeMessage;
     }
 }
